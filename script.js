1cfebd45d142fdce7da26cd43958ad4efd93bf57
Ahmed Zaghrat <zaghrat.ahmed@gmail.com>
2021-05-26 20:01:33 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let buttons = document.querySelector('.buttons');
+let btn = buttons.querySelectorAll('.btn');
+for (var i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function () {
+        let current = document.getElementsByClassName('active');
+        current[0].className = current[0].className.replace("active", "");
+        this.className += ' active';
+    });
+}
+
+
+function changeSize(size)
+{
+    document.getElementById('text').style.fontSize = size + 'em';
+}
